c95e6783e542f797613310b43ca0f51625325ec0
ywt712@gmail.com <ywt712@qq.com>
2022-07-10 22:55:06 +0800

    增加了一个方法，实现主题

--- src/main/java/film/util/SearchTextField.java
+++ src/main/java/film/util/SearchTextField.java
@@ -7,25 +7,29 @@ import java.awt.event.FocusListener;
 
 public class SearchTextField extends RoundPanel {
 
-    Color bgColor;
+    private Color colorFont;
+    private Color colorBg;
+
+    private Color searchColorFont;
+    private Color searchColorBg;
 
     JTextField jf=new JTextField();
     public SearchTextField(ImageIcon icon, int width, int height, Color bgColor) {
         super(height, height);
         if (bgColor.equals(new Color(255,255,255))) {
-            this.bgColor = new Color(214, 224, 210);
+            this.colorBg = new Color(214, 224, 210);
         } else {
-            this.bgColor = new Color(51, 50, 50);
+            this.colorBg = new Color(51, 50, 50);
         }
         this.setSize(width,height);
         this.setLayout(null);
-        this.setBackground(this.bgColor);
+        this.setBackground(this.colorBg);
         JLabel imageLabel = new JLabel(icon);
         imageLabel.setBounds(10, 5, height-10, height-10);
-        imageLabel.setBackground(this.bgColor);
+        imageLabel.setBackground(this.colorBg);
         jf.setBounds(height,0,width-height-10,height);
         jf.setBorder(BorderFactory.createEmptyBorder());
-        jf.setBackground(this.bgColor);
+        jf.setBackground(this.colorBg);
         jf.setText("输入你要查找的影片");
         jf.setFont(new Font("宋体", Font.BOLD, 18));
         if (bgColor.equals(new Color(255,255,255))) {
@@ -50,11 +54,73 @@ public class SearchTextField extends RoundPanel {
 
     }
 
+    /**
+     * @apiNote 712f
+     * @param icon
+     * @param width
+     * @param height
+     */
+    public SearchTextField(ImageIcon icon, int width, int height) {
+        super(height, height);
+//        if (bgColor.equals(new Color(255,255,255))) {
+//            this.colorBg = new Color(214, 224, 210);
+//        } else {
+//            this.colorBg = new Color(51, 50, 50);
+//        }
+        this.setSize(width,height);
+        this.setLayout(null);
+        this.setBackground(this.colorBg);
+        JLabel imageLabel = new JLabel(icon);
+        imageLabel.setBounds(10, 5, height-10, height-10);
+        imageLabel.setBackground(this.colorBg);
+        jf.setBounds(height,0,width-height-10,height);
+        jf.setBorder(BorderFactory.createEmptyBorder());
+        jf.setBackground(this.colorBg);
+        jf.setText("输入你要查找的影片");
+        jf.setFont(new Font("宋体", Font.BOLD, 18));
+//        if (bgColor.equals(new Color(255,255,255))) {
+//            jf.setForeground(new Color(0,0,0));
+//        } else {
+//            jf.setForeground(new Color(255,255,255));
+//        }
+
+        jf.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                jf.setText("");
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+
+            }
+        });
+        this.add(imageLabel);
+        this.add(jf);
+
+    }
+
     public String getJf() {
         return jf.getText();
     }
 
     public void setJf(String text){
         jf.setText(text);
+    }
+
+    public Color getColorFont() {
+        return colorFont;
+    }
+
+    public void setColorFont(Color colorFont) {
+        this.colorFont = colorFont;
+    }
+
+    public Color getColorBg() {
+        return colorBg;
+    }
+
+    public void setColorBg(Color colorBg) {
+        this.colorBg = colorBg;
     }
 }
