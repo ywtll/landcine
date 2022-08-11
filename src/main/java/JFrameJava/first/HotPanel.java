4b58bf707994d50e88f8e0c23e9c021347e08559
ywt712@gmail.com <ywt712@qq.com>
2022-08-11 17:58:48 +0800

    提高程序的稳定性，优化程序的流程度

--- src/main/java/JFrameJava/first/HotPanel.java
+++ src/main/java/JFrameJava/first/HotPanel.java
@@ -13,33 +13,59 @@ public class HotPanel extends RoundPanel {
     Font titleFont = new Font("宋体", Font.BOLD, 20);
 
     ArrayList<DZ_Movie_Hot> hotsInfo=new  ArrayList<>();
+    JLabel title;
+
     private  int index=0;
+
+    private Color enterColor=new Color(255,255,255);
+    private Color exitColor=new Color(255,255,255);
 
     public HotPanel(ImageIcon icon, int arcWidth, int arcHeight,String name) {
         super(arcWidth, arcHeight);
         this.setLayout(null);
 
         setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                setBackground(new Color(210, 210, 210));
-            }
-
-            public void mouseExited(MouseEvent e) {
-                setBackground(new Color(245, 243, 243, 255));
-            }
-        });
 
         JLabel imgLabel = new JLabel(icon);
         imgLabel.setBounds(0, 0, 220, 300);
-        //this.setSize(220,350);
         this.add(imgLabel);
-        JLabel title = new JLabel(name);
+        title = new JLabel(name);
         title.setFont(titleFont);
         title.setBounds(10, 310, 200, 20);
+
         this.add(imgLabel);
         this.add(title);
     }
+
+    public void setTitleColor(Color enterColor, Color exitColor) {
+        title.setForeground(exitColor);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                title.setForeground(enterColor);
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+                title.setForeground(exitColor);
+            }
+        });
+    }
+
+    public void setBackgroundColor(Color enterColor, Color exitColor) {
+        HotPanel h = this;
+        this.setBackground(exitColor);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                h.setBackground(enterColor);
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+                h.setBackground(exitColor);
+            }
+        });
+    }
+
+
 }
 
